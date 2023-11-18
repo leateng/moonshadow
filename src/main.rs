255d788c682d36caff9c96fa415c46f27e2ce822
teng.li <teng.li@yhlsoft.com>
2023-11-18 17:38:07 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,11 @@
+use prism::{parse, ParseResult};
+use std::collections::HashMap;
+use std::path::Path;
+use std::{fs, io};
 use tower_lsp::jsonrpc::Result;
 use tower_lsp::lsp_types::*;
 use tower_lsp::{Client, LanguageServer, LspService, Server};
+use walkdir::WalkDir;
 
 #[derive(Debug)]
 struct Backend {
@@ -24,8 +29,48 @@ impl LanguageServer for Backend {
     }
 }
 
+fn visit_ruby_files<F>(dir: &Path, mut callback: F) -> io::Result<()>
+where
+    F: FnMut(&Path),
+{
+    for entry in WalkDir::new(dir) {
+        let entry = entry?;
+        let path = entry.path();
+        if path.extension() == Some("rb".as_ref()) {
+            callback(path);
+        }
+    }
+    Ok(())
+}
+
+// fn read_file(path: &Path) -> Result<&[u8], std::io::Error> {
+//     let file_content = fs::read(path)?;
+//
+//     Ok(&file_content)
+// }
+
+struct SourceCode<'a> {
+    source: Vec<u8>,
+    parse: ParseResult<'a>,
+}
+
 #[tokio::main]
 async fn main() {
+    let mut asts = HashMap::new();
+    let project_path = Path::new("D:\\work\\CRM_NEW");
+    visit_ruby_files(project_path, |path| {
+        println!("Found ruby file: {:?}", path);
+        let content = Box::new(fs::read(path).unwrap());
+        asts.insert(
+            path.to_owned(),
+            SourceCode {
+                source: *content,
+                parse: parse(&(*content.as_slice())),
+            },
+        );
+    });
+
+    // println!("{:?}", asts.len());
     let stdin = tokio::io::stdin();
     let stdout = tokio::io::stdout();
 
